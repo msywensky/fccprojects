d6fd240f5f3edb08ddd2ae4483b120418dcf76c2
Mike Sywensky <michael.sywensky@pfizer.com>
2016-06-12 20:23:03 -0400

    adding twitch solution

--- twitch.js
+++ twitch.js
@@ -1,45 +1,145 @@
 
 
-function getListOfStreams() {
 
-	var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", 
-	"storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+class User {
 
-	$.ajax({
-		url: "https://api.twitch.tv/kraken/streams",
-	
-		// The name of the callback parameter, as specified by the YQL service
-		jsonp: "callback",
-	
-		// Tell jQuery we're expecting JSONP
-		dataType: "jsonp",
-	
-		// Tell YQL what we want and that we want JSON
-		data: {
-			channel: channels.join(",")
-		},
-	
-		// Work with the response
-		success: function( response ) {
-			$("#divResults").text(response);
-			console.log( response ); // server response
-		}
-	});
+	get name() {
+		return this._name;
+	}
+
+	set name(name) {
+		this._name = name;
+	}
+
+	get isValid() {
+		return this._valid;
+	}
+
+	set isValid(valid) {
+		this._valid = valid;
+	}
+
+	get icon() {
+		return this._icon;
+	}
+
+	set icon(icon) {
+		this._icon = icon;
+	}
+
+	get streamUrl() {
+		return this._streamUrl;
+	}
+
+	set streamUrl(streamUrl) {
+		this._streamUrl = streamUrl;
+	}
+
+	get streamName() {
+		return this._streamName;
+	}
+	set streamName(streamName) {
+		this._streamName = streamName;
+	}
+
+	get isOnline() {
+		return this._online;
+	}
+
+	set isOnline(online) {
+		this._online = online;
+	}
+
+	get viewers() {
+		return this._viewers;
+	}
+
+	set viewers(viewers) {
+		this._viewers = viewers;
+	}
+
+	constructor(name) {
+		this.name = name;
+		this.icon = "";
+		this.isOnline = false;
+		this.streamName = "Offline";
+		this.streamUrl = "";
+		this.isValid = false;
+		this.viewers = 0;
+	 }
 }
 
-function getChannelData(channel) {
-	$.getJSON('https://api.twitch.tv/kraken/streams/' + channel + '?callback=?', function(data) {
+var users = [];
+function loadUsers() {
+	users = [];
+	users.push(new User("InvalidUserXXx"));
+	users.push(new User("ESL_SC2"));
+	users.push(new User("cretetion"));
+	users.push(new User("freecodecamp"));
+	users.push(new User("storbeck"));
+	users.push(new User("habathcx"));
+	users.push(new User("RobotCaleb"));
+	users.push(new User("noobs2ninjas"));
+	users.push(new User("OgamingSC2"));
+
+	return $.map(users,getUserInfo);
+}
+
+function loadStreams() {
+	console.log("calling loadStreamms");
+	var filtered = users.filter((item)=>item.isValid);
+	console.log("Filtered len: " + filtered.length);
+	return $.map(filtered,getStreamInfo);
+}
+
+function getUserInfo(user) {
+	return $.getJSON('https://api.twitch.tv/kraken/users/' + user.name + '?callback=?', function(data) {
 		console.log(data);
-	});
+		if ( (data["status"] !== undefined) && (data["status"] === 404) ) {
+			user.streamName = "User not found";
+
+		} else {
+			if (data["logo"]!== undefined) {
+				user.icon = data.logo;
+			}
+			user.isValid = true;
+		}
+
+	}).promise();
 }
 
-function getChD() {
-	var data = $("#txtData").val();
-	console.log(data);
-	getChannelData(data);
+function getStreamInfo(user) {
+	console.log("getStreamInfoCalled:" + user.name);
+	return $.getJSON('https://api.twitch.tv/kraken/streams/' + user.name + '?callback=?', function(data) {
+		console.log(data);
+		if ( (data["stream"]) && (data["stream"] != null) ) {
+			user.streamUrl = data.stream.channel.url;
+			user.isOnline = true;
+			user.streamName = data.stream.game;
+			user.viewers = data.stream.viewers;
+		}
+	}).promise();
+}
+
+function runTemplate() {
+	$.Mustache.addFromDom('channel-template');
+	$('#divResults').mustache('channel-template',{data:users});
+}
+
+function gotoStream(stream) {
+	window.open(stream,"_blank");
+}
+
+function sortList() {
+	users.sort((a,b)=>(b.isOnline - a.isOnline));
+	console.log("sortList run");
 }
 
 $(document).ready( function () {
-	$("#btnTest").on("click",getListOfStreams);
-	$("#btnGetDetail").on("click", getChD);
+	$("#btnRunTemplate").on("click",runTemplate);
+	var promises = loadUsers();
+	$.when.apply(null, promises).then(function(data) {
+		var promise2 = loadStreams();
+		$.when.apply(null, promise2).then(()=>{sortList();console.log("done");runTemplate();});
+	})
 });
